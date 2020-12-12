cc15d92696301fbbc54609560cc5b8efec162569
Fasih Mehmood <48308392+fasih-mehmood@users.noreply.github.com>
2020-12-12 19:08:40 +0500

    Our work section completed

--- js/script.js
+++ js/script.js
@@ -20,6 +20,9 @@ $(document).ready(function () {
     $("nav ul").toggleClass("hidden");
   });
 
+  if(window.innerWidth > 1024 && window.scrollY > 102) {
+    $(".site-header").addClass("fixed float-header");
+  }
   // change header on scroll
   $(window).scroll(function () {
     if (window.innerWidth > 1024) {
@@ -33,6 +36,29 @@ $(document).ready(function () {
       }
     }
   });
+
+  // initialize isotop
+  var $flist = $("#filter-items");
+  var $filter = $("#filters")
+  // Run Isotope
+  $flist.isotope({
+    filter: "*",
+    itemSelector: '.filter-item'
+  })
+  $filter.find("a").click(function() {
+    var selector = $(this).attr("data-filter");
+    $(this).parent().parent().find("a").removeClass("active");
+    $(this).addClass("active");
+    $flist.isotope({
+      filter: selector,
+      animationOptions: {
+          duration: 750,
+          easing: "linear",
+          queue: false,
+      },
+    });
+    return false;
+    });
 });
 
 
