2bbd358cedd3cb96060e7a059384342df1c33940
Fasih Mehmood <48308392+fasih-mehmood@users.noreply.github.com>
2020-12-16 15:30:11 +0500

    All sections completed

--- js/script.js
+++ js/script.js
@@ -20,11 +20,27 @@ $(document).ready(function () {
     $("nav ul").toggleClass("hidden");
   });
 
+  //scroll to top
+  $(".go-top").click(e => {
+    $("body").animate({scrollTop:0}, '500');
+  });
+
   if(window.innerWidth > 1024 && window.scrollY > 102) {
     $(".site-header").addClass("fixed float-header");
   }
+
+  if(window.scrollY > 815) {
+    $(".go-top").addClass("show");
+  }
+
   // change header on scroll
   $(window).scroll(function () {
+    if(window.scrollY > 815) {
+      $(".go-top").addClass("show");
+    } else {
+      $(".go-top").removeClass("show");
+    }
+
     if (window.innerWidth > 1024) {
       if (window.scrollY <= 102 && window.scrollY != 0) {
         $('.site-header').removeClass("float-header");
@@ -36,6 +52,50 @@ $(document).ready(function () {
       }
     }
   });
+
+  /* Navigation scroll */
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ?
+          target :
+          $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate({
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
 
   // initialize isotop
   var $flist = $("#filter-items");
@@ -61,31 +121,3 @@ $(document).ready(function () {
     return false;
     });
 });
-
-
-// $(document).on('scroll', function () {
-//   $(".counter-count").each(function () {
-//     $(this).prop('Counter', 0).animate({
-//       Counter: $(this).text()
-//     }, {
-//       duration: 2000,
-//       easing: 'swing',
-//       step: function (now) {
-//         $(this).text(Math.ceil(now));
-//       }
-//     });
-//     var counter = 1;
-//   }).isOnScreen();
-// })
-
-// $('.counter-count').each(function () {
-//   $(this).prop('Counter', 0).animate({
-//     Counter: $(this).text()
-//   }, {
-//     duration: 2000,
-//     easing: 'swing',
-//     step: function (now) {
-//       $(this).text(Math.ceil(now));
-//     }
-//   });
-// });
